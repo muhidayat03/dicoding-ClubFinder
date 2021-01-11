ead01d3e3f6e9df730e3e39e043e6cae1b1cc1c8
Muhammad Hidayat <muhidayat03@gmail.com>
2021-01-11 15:39:48 +0700

    add shadow dom

--- src/script/components/club-item.js
+++ src/script/components/club-item.js
@@ -1,17 +1,55 @@
 class ClubItem extends HTMLElement {
+
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
   set club(club) {
     this._club = club;
     this.render();
   }
 
   render() {
- 
-          this.innerHTML = `
-          <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
-          <div class="club-info">
-              <h2>${this._club.name}</h2>
-              <p>${this._club.description}</p>
-          </div>`;
+
+    this.shadowDOM.innerHTML = `
+      <style>
+        :host {
+          margin-bottom: 18px;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          border-radius: 10px;
+          overflow: hidden;
+          display: block;
+        }
+
+        .fan-art-club {
+          width: 100%;
+          max-height: 300px;
+          object-fit: cover;
+          object-position: center;
+        }
+
+        .club-info {
+          padding: 24px;
+        }
+
+        h2 {
+          font-weight: lighter;
+        }
+
+        p {
+          margin-top: 10px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 10; /* number of lines to show */
+        }
+      </style>
+      <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+      <div class="club-info">
+          <h2>${this._club.name}</h2>
+          <p>${this._club.description}</p>
+      </div>`;
   }
 
 }
