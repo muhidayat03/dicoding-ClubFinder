66229d39370abde1cb959887158a61aa0a9d486a
Muhammad Hidayat <muhidayat03@gmail.com>
2021-01-11 17:32:02 +0700

    add webpack config

--- app.js
+++
@@ -1,7 +0,0 @@
-import main from './src/script/view/main.js';
-import './src/script/components/app-bar.js';
-import './src/script/components/search-bar.js';
-import './src/script/components/club-list.js';
-import './src/script/components/club-item.js';
-
-document.addEventListener("DOMContentLoaded", main);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import main from './script/view/main';
+import './script/components/app-bar';
+import './script/components/search-bar';
+import './script/components/club-list';
+import './script/components/club-item';
+import "./styles/style.css"; 
+
+document.addEventListener("DOMContentLoaded", main);
